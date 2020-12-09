1b6dfbde13c279cf31f702012948f998cf022acc
Saamer Mansoor <i@saamer.me>
2020-12-09 14:36:57 -0500

    Updated the sample with a simple solution

--- README.md
+++ README.md
@@ -3,12 +3,8 @@
 
 ## Steps to implement it
 
-Basically I created a UIView that contains an AVPlayer and I added that to the View of my ViewController.
-
-* Create a PlayerView.cs file in the iOS project, and paste in the code there from this repo, ensuring that you change the name space to your name space
-* Then create MainPageRenderer.cs in the iOS project, a custom PageRenderer for the MainPage and override the lifecycle functions so that you can add the PlayerView to your ViewController
-* Ensure that your target is at least iOS 10 in the iOS project settings
-* In the info.plist, set UIBackgroundMode to audio
+* Follow the instructions on the Microsoft Xamarin official tutorial to setup a page displaying a video
+* Then add in the info.plist, set UIBackgroundMode to audio
 * In the AppDelegate.cs file, set audio session category to AVAudioSessionCategoryPlayback or AVAudioSessionCategoryPlayAndRecord
 
 ### Notes about testing while development
---
+++ Sample/Sample.iOS/VideoPlayerRenderer.cs
@@ -0,0 +1,61 @@
+﻿using AVFoundation;
+using AVKit;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Foundation;
+using Sample;
+
+[assembly: ExportRenderer(typeof(VideoPlayerView), typeof(Sample.iOS.VideoPlayerRenderer))]
+namespace Sample.iOS
+{
+
+    public class VideoPlayerRenderer : ViewRenderer<VideoPlayerView, UIView>
+    {
+        AVPlayer _player;
+        AVPlayerViewController _playerViewController;
+        VideoPlayerView _videoPlayer;
+
+        public override UIViewController ViewController => _playerViewController;
+
+        public VideoPlayerRenderer()
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<VideoPlayerView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    // Create AVPlayerViewController
+                    _playerViewController = new AVPlayerViewController()
+                    {
+                    };
+                    
+                    // Set Player property to AVPlayer
+                    _player = new AVPlayer(new NSUrl(Element.Source));
+                    _playerViewController.Player = _player;
+
+                    // Use the View from the controller as the native control
+                    SetNativeControl(_playerViewController.View);
+                }
+
+                _videoPlayer = e.NewElement;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (_player != null)
+            {
+                _player.ReplaceCurrentItemWithPlayerItem(null);
+            }
+        }
+
+    }
+}
